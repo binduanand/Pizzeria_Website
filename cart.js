1c93fd45eedf2d48639adcc7cd1b605a5d87b97c
Bindu Anand <137068272+binduanand@users.noreply.github.com>
2024-03-20 23:37:13 +0530

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,103 @@
+const addToCartButtons = document.querySelectorAll(".add_to_cart");
+let cartCount = parseInt(document.querySelector(".cart-count").innerText);
+let cartContent = document.querySelector(".cart-modal-content");
+let quantityBtns = document.querySelectorAll(".quantity");
+let items = document.querySelectorAll(".item");
+let increaseBtns = document.querySelectorAll(".increase-quantity");
+let decreaseBtns = document.querySelectorAll(".decrease-quantity");
+let quantityvalues = document.querySelectorAll(".quantity span");
+
+let costArray = [];
+document.querySelectorAll(".cost").forEach((costElement)=> {
+    costArray.push(costElement.innerText);
+});
+
+console.log(costArray);
+
+let itemQuantities = new Array(items.length).fill(0);
+
+addToCartButtons.forEach(function(button, index) {
+    button.addEventListener("click", function() {
+        this.style.display = "none";
+        quantityBtns[index].style.display = "flex";
+
+        cartCount++;
+        document.querySelector(".cart-count").innerText = cartCount;
+
+        itemQuantities[index]++;
+        updateCart();
+    });
+});
+
+increaseBtns.forEach((button, index) => {
+    button.addEventListener("click",() => {
+        increaseQuantity(index);
+    });
+});
+
+decreaseBtns.forEach((button, index) => {
+    button.addEventListener("click", () =>{
+        decreaseQuantity(index);
+    });
+});
+
+function increaseQuantity(index) {
+    let value = parseInt(quantityvalues[index].innerText);
+    value++;
+    quantityvalues[index].innerText = value;
+    itemQuantities[index]++;
+    updateCart();
+}
+
+function decreaseQuantity(index) {
+    let value = parseInt(quantityvalues[index].innerText);
+    if (value > 1) {
+        value--;
+        quantityvalues[index].innerText = value;
+        itemQuantities[index]--;
+        updateCart();
+    } else {
+        quantityBtns[index].style.display = "none";
+        addToCartButtons[index].style.display = "block";
+        cartCount--;
+        document.querySelector(".cart-count").innerText = cartCount;
+        document.querySelector(".cart-item").remove();
+        itemQuantities[index] = 0;
+        updateCart();
+    }
+}
+
+
+function cost(index){
+    let finalcost = 0;
+    for (let i = 0; i < items.length; i++) {
+        finalcost += costArray[i] * itemQuantities[i];
+    }
+    document.querySelector(".final-cost").innerText = "₹" + finalcost;
+}
+
+function updateCart() {
+    if (itemQuantities.some(quantity => quantity > 0)) {
+        cartContent.innerHTML = "";
+        itemQuantities.forEach((quantity, index) => {
+            if (quantity > 0) {
+                cartContent.innerHTML += "<div class='cart-item'>" + items[index].querySelector("figure").innerHTML + "<span class='quantity'>" + quantity + "</span></div>";
+                cost(index);
+                document.querySelector(".place-order-section").style.display = "flex";
+            }
+        });
+        
+    } else {
+        cartContent.innerHTML = "IS EMPTY <button id='start-shopping'>Add items</button>";
+        document.querySelector(".place-order-section").style.display = "none";
+    }
+}
+
+document.querySelector(".place-order").onclick = () => {
+    cartContent.innerHTML = "<div class='order-placed'> <i class='fa-solid fa-circle-check'></i> <h3>Order Placed<h3></div>"
+}   
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,79 @@
+
+// offcanvas
+const navBar = document.querySelector(".nav-bar");
+const hamburger = document.querySelector(".hamburger-icon");
+hamburger.onclick = function() {
+    navBar.classList.toggle("active");
+};
+
+// offcanvas
+
+//login modal
+let loginBtn = document.querySelector("#login-btn");
+let signUpBtn = document.querySelector("#signup-btn");
+let alAcc = document.querySelector("#already-account");
+signUpBtn.onclick= () => {
+    signUpBtn.classList.add("btn-active");
+    loginBtn.classList.remove("btn-active");
+    document.querySelector(".signup-form").style.display = "flex";
+    document.querySelector(".login-form").style.display = "none";
+};
+const switchToLogin = () => {
+    signUpBtn.classList.remove("btn-active");
+    loginBtn.classList.add("btn-active");
+    document.querySelector(".signup-form").style.display = "none";
+    document.querySelector(".login-form").style.display = "flex";
+};
+loginBtn.onclick = switchToLogin;
+alAcc.onclick = switchToLogin;
+
+const openLoginModal = document.querySelector(".login-signup");
+openLoginModal.onclick = () => {
+    document.querySelector(".modal").classList.add("open-modal");
+    document.querySelector(".hero-section").style.filter = "blur(5px)";
+    document.querySelector("nav").style.filter = "blur(5px)";
+}
+
+const closeLoginModal = document.getElementById("close-btn");
+const closeModal = () => {
+    document.querySelector(".modal").classList.remove("open-modal");
+    document.querySelector(".cart-modal").classList.remove("open-modal");
+    document.querySelector(".hero-section").style.filter = "none";
+    document.querySelector("nav").style.filter = "none";
+}
+closeLoginModal.onclick = closeModal;
+
+//login modal
+
+//cart modal
+let cartBtn = document.querySelector(".cart");
+cartBtn.onclick = () => {
+    document.querySelector(".cart-modal").classList.add("open-modal");
+}
+
+document.querySelector("#cart-close-btn").onclick = closeModal;
+document.querySelector("#start-shopping").onclick = closeModal;
+//cart modal
+
+//reviews
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+      breakpoints: {
+        450: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+      },
+  });
+//reviews
+
+
+
+
+
